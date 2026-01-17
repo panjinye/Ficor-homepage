34720c35a6d76b0eb5977428f89a8e38fe72d0fc
panjinye <panjinye@gmail.com>
2026-01-17 16:13:48 +0800

    update: 优化Vite项目首页样式，修复路由跳转bug

---
+++ src/components/Links.vue
@@ -0,0 +1,182 @@
+<template>
+  <div v-if="siteLinks[0]" class="links">
+    <div class="line">
+      <Icon size="20">
+        <Link />
+      </Icon>
+      <span class="title">网站列表</span>
+    </div>
+    <!-- 网站列表 -->
+    <Swiper
+      v-if="siteLinks[0]"
+      :modules="[Pagination, Mousewheel]"
+      :slides-per-view="1"
+      :space-between="40"
+      :pagination="{
+        el: '.swiper-pagination',
+        clickable: true,
+        bulletElement: 'div',
+      }"
+      :mousewheel="true"
+    >
+      <SwiperSlide v-for="site in siteLinksList" :key="site">
+        <el-row class="link-all" :gutter="20">
+          <el-col v-for="(item, index) in site" :span="8" :key="item">
+            <div
+              class="item cards"
+              :style="index < 3 ? 'margin-bottom: 20px' : null"
+              @click="jumpLink(item)"
+            >
+              <Icon size="26">
+                <component :is="siteIcon[item.icon]" />
+              </Icon>
+              <span class="name text-hidden">{{ item.name }}</span>
+            </div>
+          </el-col>
+        </el-row>
+      </SwiperSlide>
+      <div class="swiper-pagination" />
+    </Swiper>
+  </div>
+</template>
+
+<script setup>
+import { Icon } from "@vicons/utils";
+// 可前往 https://www.xicons.org 自行挑选并在此处引入
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Adjust, Algolia, Apple, AppleAlt, Archive, Artstation, At, Atom, Award, Bandcamp, BasketballBall, Bell, Bolt, BlackTie, Bookmark, Box, BreadSlice, Brush, Buffer, Burn, Bullseye, 
+  Calendar, Camera, Campground, Cannabis, Car, Carrot, Centercode, Check, Cheese, Clipboard, Clock, Clone, Code, CommentDots, Copy, Crown, Cuttlefish, Dashcube, Database, Delicious, Desktop, DiceD6, Docker, Dove, Edge, Edit, Eject, Envelope, Envira, Eye, FacebookF, Fan, Feather, FeatherAlt, FileAlt, Film, FirstOrderAlt, Fish, Flag, 
+  Gift, Glasses, Globe, Gofore, Gratipay, Gripfire, Hashtag, HatCowboy, HatWizard, Headphones, Heart, HeartRegular, Highlighter, Home, History, IceCream, Image, Instalod, ItunesNote, Key, Keyboard, Laptop, Leaf, Lemon, Linux, List, ListAlt, LocationArrow, Magnet, MapMarkedAlt, Marker, Mastodon, Meteor, Microphone, Mitten, MobileAlt, Moon, Music, PaintBrush, PaperPlane, PaperPlaneRegular, Paperclip, Paste, Pen, PepperHot, Plane, Qq, Redhat, Rocket, 
+  Rss, SatelliteDish, Save, Snowflake, SnowflakeRegular, SpaceShuttle, Star, Stamp, StarOfDavid, StarRegular, Store, Sun, SunRegular, Tag, Tags, TelegramPlane, Train, Tree, Umbrella, User, UserRegular, Viadeo, Video, VimeoV, Vine, Wallet, Weibo, Weixin, Wifi, Wordpress, YinYang, Zhihu } from "@vicons/fa"; // 注意使用正确的类别
+import { mainStore } from "@/store";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Mousewheel } from "swiper/modules";
+import siteLinks from "@/assets/siteLinks.json";
+
+const store = mainStore();
+
+// 计算网站链接
+const siteLinksList = computed(() => {
+  const result = [];
+  for (let i = 0; i < siteLinks.length; i += 6) {
+    const subArr = siteLinks.slice(i, i + 6);
+    result.push(subArr);
+  }
+  return result;
+});
+
+// 网站链接图标
+const siteIcon = {
+  Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Adjust, Algolia, Apple, AppleAlt, Archive, Artstation, At, Atom, Award, Bandcamp, BasketballBall, Bell, Bolt, BlackTie, Bookmark, Box, BreadSlice, Brush, Buffer, Burn, Bullseye, 
+  Calendar, Camera, Campground, Cannabis, Car, Carrot, Centercode, Check, Cheese, Clipboard, Clock, Clone, Code, CommentDots, Copy, Crown, Cuttlefish, Dashcube, Database, Delicious, Desktop, DiceD6, Docker, Dove, Edge, Edit, Eject, Envelope, Envira, Eye, FacebookF, Fan, Feather, FeatherAlt, FileAlt, Film, FirstOrderAlt, Fish, Flag, 
+  Gift, Glasses, Globe, Gofore, Gratipay, Gripfire, Hashtag, HatCowboy, HatWizard, Headphones, Heart, HeartRegular, Highlighter, Home, History, IceCream, Image, Instalod, ItunesNote, Key, Keyboard, Laptop, Leaf, Lemon, Linux, List, ListAlt, LocationArrow, Magnet, MapMarkedAlt, Marker, Mastodon, Meteor, Microphone, Mitten, MobileAlt, Moon, Music, PaintBrush, PaperPlane, PaperPlaneRegular, Paperclip, Paste, Pen, PepperHot, Plane, Qq, Redhat, Rocket, 
+  Rss, SatelliteDish, Save, Snowflake, SnowflakeRegular, SpaceShuttle, Star, Stamp, StarOfDavid, StarRegular, Store, Sun, SunRegular, Tag, Tags, TelegramPlane, Train, Tree, Umbrella, User, UserRegular, Viadeo, Video, VimeoV, Vine, Wallet, Weibo, Weixin, Wifi, Wordpress, YinYang, Zhihu
+};
+
+// 链接跳转
+const jumpLink = (data) => {
+  if (data.name === "音乐" && store.musicClick) {
+    if (typeof $openList === "function") $openList();
+  } else {
+    window.open(data.link, "_blank");
+  }
+};
+
+onMounted(() => {
+  console.log(siteLinks);
+});
+</script>
+
+<style lang="scss" scoped>
+.links {
+  .line {
+    margin: 2rem 0.25rem 1rem;
+    font-size: 1.1rem;
+    display: flex;
+    align-items: center;
+    animation: fade 0.5s;
+    .title {
+      margin-left: 8px;
+      font-size: 1.15rem;
+      text-shadow: 0 0 5px #00000050;
+    }
+  }
+  .swiper {
+    left: -10px;
+    width: calc(100% + 20px);
+    padding: 5px 10px 0;
+    z-index: 0;
+    .swiper-slide {
+      height: 100%;
+    }
+    .swiper-pagination {
+      margin-top: 12px;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      :deep(.swiper-pagination-bullet) {
+        background-color: #fff;
+        width: 20px;
+        height: 4px;
+        margin: 0 4px;
+        border-radius: 4px;
+        opacity: 0.2;
+        transition: opacity 0.3s;
+        &.swiper-pagination-bullet-active {
+          opacity: 1;
+        }
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
+  .link-all {
+    height: 220px;
+    .item {
+      height: 100px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      justify-content: center;
+      padding: 0 10px;
+      animation: fade 0.5s;
+
+      &:hover {
+        transform: scale(1.02);
+        background: rgb(0 0 0 / 40%);
+        transition: 0.3s;
+      }
+
+      &:active {
+        transform: scale(1);
+      }
+
+      .name {
+        font-size: 1.1rem;
+        margin-left: 8px;
+      }
+      @media (min-width: 720px) and (max-width: 820px) {
+        .name {
+          display: none;
+        }
+      }
+      @media (max-width: 720px) {
+        height: 80px;
+      }
+      @media (max-width: 460px) {
+        flex-direction: column;
+        .name {
+          font-size: 1rem;
+          margin-left: 0;
+          margin-top: 8px;
+        }
+      }
+    }
+    @media (max-width: 720px) {
+      height: 180px;
+    }
+  }
+}
+</style>
